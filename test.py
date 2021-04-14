196ed05de209afa5f93e2ccd85318d6fbdd86677
dangzhangjing <dangzhangjing@baijia.com>
2021-04-14 20:31:00 +0800

    元组学习

--- test.py
+++ test.py
@@ -1,3 +1,23 @@
-list=[1,2,3,4,5]
-for i in range(len(list)):
-    print("索引为：{},数值为：{}".format(i,list[i]))
+
+def index():
+    for i in range(len(my_tupple)):
+        print("索引分别为：{}".format(i))
+    print('\n')
+
+def data():
+    for i in my_tupple:
+        print("数值分别为：{}".format(i))
+    print('\n')
+
+
+def index_data():
+    for i in range(len(my_tupple)):
+        print("索引是：{},索引对应的数值是：{}".format(i,my_tupple[i]))
+    print('\n')
+
+
+
+my_tupple=(1,2,3,4)
+index()
+data()
+index_data()
